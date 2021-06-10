19c895bff585ccdc63b24b65a9d6c226ba73bb45
maxevdo56 <Maxevdo56@yandex.ru>
2021-06-10 22:56:26 +0300

    DZ 2 (1-4 zadanie)

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ echo '<br>';
 $c = ($a >= 0 && $b >= 0) ? ($a - $b) : (($a < 0 && $b < 0) ? ($a * $b) : ($a + $b));
 echo $c;
 echo '<br>';
+echo '<br>';
 
 /* 2. Присвоить переменной $а значение в промежутке [0..15]. 
 С помощью оператора switch организовать вывод чисел от $a до 15.*/
@@ -43,7 +44,7 @@ Switch ($a) {
     Case 14: echo '14'.'<br>';
     Default: echo '15'.'<br>';
 }
-
+    echo '<br>';
 /* 3. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. 
 Обязательно использовать оператор return.*/
 
@@ -68,10 +69,63 @@ echo multipl(8, 3);
 echo '<br>';
 echo divide(8, 3);
 echo '<br>';
+echo '<br>';
 
 /* 4. Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation), 
 где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. 
 В зависимости от переданного значения операции выполнить одну из арифметических операций
 (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
 */
+
+function mathOperation($arg1, $arg2, $operation) {
+    Switch ($operation) {
+        Case 'sum': $result = sum($arg1, $arg2); Break;
+        Case 'differ': $result = differ($arg1, $arg2); Break;
+        Case 'multipl': $result = multipl($arg1, $arg2); Break;
+        Case 'divide': $result = divide($arg1, $arg2); Break;
+        Default: echo 'Введите правильную операцию';
+    };
+    return $result;
+};
+echo 'Функция mathOperation($arg1, $arg2, $operation);';
+echo '<br>';
+echo mathOperation(9, 3, 'sum');
+echo '<br>';
+echo mathOperation(9, 3, 'differ');
+echo '<br>';
+echo mathOperation(9, 3, 'multipl');
+echo '<br>';
+echo mathOperation(9, 3, 'divide');
+echo '<br>';
+echo '<br>';
+
+/* 5. Посмотреть на встроенные функции PHP. Используя имеющийся HTML-шаблон, 
+вывести текущий год в подвале при помощи встроенных функций PHP. */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>Текущий год <?php echo date('Y')?></footer>
+</body>
+</html>
+
+<?php
+/* 
+6. *С помощью рекурсии организовать функцию возведения числа в степень. 
+Формат: function power($val, $pow), где $val – заданное число, $pow – степень.  */
+echo '<br>';
+echo '<br>';
+function power($val, $pow) {
+    if ($pow = 0) return 1;
+    return ($val * power($val, $pow - 1));
+};
+
+echo power(2, 3);
 ?>
